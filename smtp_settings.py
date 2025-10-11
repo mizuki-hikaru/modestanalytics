41394cb992349c0b0fd383e556c827d3a909c9bc
Mizuki Hikaru <mizuki@hikaru.org>
2025-10-11 19:18:03 +0000

    update smtp_settings.py to not include faulty documentation

--- smtp_settings.py
+++ smtp_settings.py
@@ -1,8 +1,8 @@
 # Fill these with your SMTP provider details
 SMTP_HOST = "smtp.postmarkapp.com"
-SMTP_PORT = 587  # SSL port
-SMTP_USERNAME = "your-smtp-username"  # leave empty if not required
-SMTP_PASSWORD = "your-smtp-password"  # leave empty if not required
+SMTP_PORT = 587
+SMTP_USERNAME = "your-smtp-username"
+SMTP_PASSWORD = "your-smtp-password"
 
 # The visible From address for outgoing emails
 SMTP_FROM = "Modest Analytics <no-reply@modestanalytics.com>"
